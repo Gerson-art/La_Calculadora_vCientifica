00fe72ec2f4a348324ec7d44ab12cb7cf2adaa96
GersonUA <g.urrea02@ufromail.cl>
2023-09-21 18:25:00 -0300

    Versión final de Ecuación de la Recta.

--- Ecuacion_recta.java
+++ Ecuacion_recta.java
@@ -52,6 +52,6 @@ public class Ecuacion_recta {
         double m = (b[1] - a[1]) / (b[0] - a[0]);
         double coeficiente = m * (-a[0]) + a[1];
         System.out.println("La ecuación de la recta es: Y = " + m + "x +" + coeficiente);
-
+        /**/
     }
 }
